1cc63fe3db8c36404172eefe8504eff6a4fcd711
martin bedouret <martinbedouret@gmail.com>
2018-01-09 18:30:10 -0300

    add some necessary updates for authentication

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+var swaggerTools = require('swagger-tools');
+var YAML = require('yamljs');
 const config = require('./config');
 module.exports = app; // for testing
 
 var swaggerConfig = {
   appRoot: __dirname // required config,
-}
+};
 
 /*bbdd configuration in its own file*/
 require('./db');
@@ -21,7 +23,4 @@ SwaggerExpress.create(swaggerConfig, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
 });
