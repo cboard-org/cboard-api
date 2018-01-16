7c089c47695e72422ef692ae47857585bce84883
martin bedouret <martinbedouret@gmail.com>
2018-01-16 17:38:08 -0300

    upda production db

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: 'development',
-  databaseUrl: process.env.MONGO_URL || 'mongodb://localhost/cboard-api',
+  databaseUrl: process.env.MONGO_URL || 'mongodb://martinbedouret:Muni1909@ds253587.mlab.com:53587/cboard',
   jwt: { secret: process.env.JWT_SECRET || 'secret key for testing' }
 };
