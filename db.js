2ce4cacf8f538ac7cad903921d12ad6c2bbe1776
martin bedouret <martinbedouret@gmail.com>
2018-01-17 09:26:18 -0300

    fix

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var config = require('./config');
 
 mongoose.connect('mongodb://martinbedouret:Muni1909@ds253587.mlab.com:53587/cboard');
-mongoose.connection.on('connected', () => console.log('Connected to database: ' + config.databaseUrl));
+mongoose.connection.on('connected', () => console.log('Connected to database '));
 mongoose.connection.on('error',(err) => console.log('Database connection error: ' + err));
 mongoose.connection.on('disconnected', () => console.log('Disconnected from database'));
 
