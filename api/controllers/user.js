f4cf488ec7adcddf51f6940de8f9b0c94bb1ed77
martin bedouret <martinbedouret@gmail.com>
2018-01-29 17:54:50 -0300

    use bycryptjs, update emails

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,222 +1,224 @@
-var User = require('../models/User');
-var mailing = require('../mail');
-var nev = mailing('en');
+var User = require("../models/User");
+var mailing = require("../mail");
+var nev = mailing("en");
 var auth = require("../helpers/auth");
 
 module.exports = {
-    createUser: createUser,
-    activateUser: activateUser,
-    listUser: listUser,
-    removeUser: removeUser,
-    getUser: getUser,
-    updateUser: updateUser,
-    loginUser: loginUser,
-    logoutUser: logoutUser
+  createUser: createUser,
+  activateUser: activateUser,
+  listUser: listUser,
+  removeUser: removeUser,
+  getUser: getUser,
+  updateUser: updateUser,
+  loginUser: loginUser,
+  logoutUser: logoutUser
 };
 
 function createUser(req, res) {
-    var user = new User(req.body);
+  var user = new User(req.body);
 
-    nev.createTempUser(user, function (err, existingPersistentUser, newTempUser) {
-
+  nev.createTempUser(user, function(err, existingPersistentUser, newTempUser) {
+    if (err) {
+      return res.status(404).json({
+        message: err
+      });
+    }
+    // user already exists in persistent collection
+    if (existingPersistentUser) {
+      return res.status(409).json({
+        message:
+          "You have already signed up and confirmed your account. Did you forget your password?"
+      });
+    }
+    // new user created
+    if (newTempUser) {
+      var URL = newTempUser[nev.options.URLFieldName];
+      nev.sendVerificationEmail(newTempUser.email, URL, function(err, info) {
         if (err) {
-            return res.status(404).json({
-                message: err
-            });
+          return res.status(500).json({
+            message: "ERROR: sending verification email FAILED " + info
+          });
         }
-        // user already exists in persistent collection
-        if (existingPersistentUser) {
-            return res.status(409).json({
-                message: 'You have already signed up and confirmed your account. Did you forget your password?'
-            });
-        }
-        // new user created
-        if (newTempUser) {
-            var URL = newTempUser[nev.options.URLFieldName];
-            nev.sendVerificationEmail(newTempUser.email, URL, function (err, info) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'ERROR: sending verification email FAILED ' + info
-                    });
-                }
-            });
+      });
 
-            return res.status(200).json({
-                success: 1,
-                message: 'An email has been sent to you. Please check it to verify your account.'
-            });
+      return res.status(200).json({
+        success: 1,
+        message:
+          "An email has been sent to you. Please check it to verify your account."
+      });
 
-            // user already exists in temporary collection!
-        } else {
-            return res.status(409).json({
-                message: 'You have already signed up. Please check your email to verify your account.'
-            });
-        }
-    });
+      // user already exists in temporary collection!
+    } else {
+      return res.status(409).json({
+        message:
+          "You have already signed up. Please check your email to verify your account."
+      });
+    }
+  });
 }
 function activateUser(req, res) {
-    var url = req.swagger.params.url.value;
-    nev.confirmTempUser(url, function (err, user) {
-        if (user) {
-            nev.sendConfirmationEmail(user.email, function (err, info) {
-                if (err) {
-                    return res.status(404).json({
-                        message: 'ERROR: sending confirmation email FAILED ' + info
-                    });
-                }
-                return res.status(200).json({
-                    success: 1,
-                    message: 'CONFIRMED!'
-                });
-            });
-        } else {
-            return res.status(404).json({
-                message: 'ERROR: confirming temp user FAILED ' + err
-            });
+  var url = req.swagger.params.url.value;
+  nev.confirmTempUser(url, function(err, user) {
+    if (user) {
+      nev.sendConfirmationEmail(user.email, function(err, info) {
+        if (err) {
+          return res.status(404).json({
+            message: "ERROR: sending confirmation email FAILED " + info
+          });
         }
-    });
+        return res.status(200).json({
+          success: 1,
+          message: "CONFIRMED!"
+        });
+      });
+    } else {
+      return res.status(404).json({
+        message: "ERROR: confirming temp user FAILED " + err
+      });
+    }
+  });
 }
 function listUser(req, res) {
-    User.find(function (err, Users) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Error getting user list. ' + err
-            });
-        }
-        return res.status(200).json(Users);
-    });
+  User.find(function(err, Users) {
+    if (err) {
+      return res.status(500).json({
+        message: "Error getting user list. " + err
+      });
+    }
+    return res.status(200).json(Users);
+  });
 }
 function removeUser(req, res) {
-    var id = req.swagger.params.id.value;
-    User.findByIdAndRemove(id, function (err, users) {
-        if (err) {
-            return res.status(404).json({
-                message: 'User not found. User Id: ' + id
-            });
-        }
-        return res.status(200).json(users);
-    });
+  var id = req.swagger.params.id.value;
+  User.findByIdAndRemove(id, function(err, users) {
+    if (err) {
+      return res.status(404).json({
+        message: "User not found. User Id: " + id
+      });
+    }
+    return res.status(200).json(users);
+  });
 }
 function getUser(req, res) {
-    var id = req.swagger.params.id.value;
-    User.findOne({_id: id}, function (err, users) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Error getting user. ' + err
-            });
-        }
-        if (!users) {
-            return res.status(404).json({
-                message: 'User does not exist. User Id: ' + id
-            });
-        }
-        return res.status(200).json(users);
-    });
+  var id = req.swagger.params.id.value;
+  User.findOne({ _id: id }, function(err, users) {
+    if (err) {
+      return res.status(500).json({
+        message: "Error getting user. " + err
+      });
+    }
+    if (!users) {
+      return res.status(404).json({
+        message: "User does not exist. User Id: " + id
+      });
+    }
+    return res.status(200).json(users);
+  });
 }
 function updateUser(req, res) {
-    var id = req.swagger.params.id.value;
-    User.findOne({_id: id}, function (err, users) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Error updating user. ' + err
-            });
-        }
-        if (!users) {
-            return res.status(404).json({
-                message: 'Unable to find user. User Id: ' + id
-            });
-        }
-        users.name = req.body.name;
-        users.username = req.body.username;
-        users.email = req.body.email;
-        users.locale = req.body.locale;
-        users.save(function (err, users) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error saving user ' + err
-                });
-            }
-            if (!users) {
-                return res.status(404).json({
-                    message: 'Unable to find user. User id: ' + id
-                });
-            }
-        });
-        return res.status(200).json(users);
-    });
-}
-function loginUser(req, res) {
-    var role = req.swagger.params.role.value;
-    var username = req.body.username;
-    var password = req.body.password;
-
-    if (role !== "user" && role !== "admin") {
-        return res.status(400).json({
-            message: "Error: Role must be either admin or user"
-        });
+  var id = req.swagger.params.id.value;
+  User.findOne({ _id: id }, function(err, users) {
+    if (err) {
+      return res.status(500).json({
+        message: "Error updating user. " + err
+      });
     }
-    User.authenticate(username, password, function (error, user) {
-      if (error || !user) {
-          return res.status(401).json({
-             message: "Wrong email or password."
-         });
-      } else {
-        req.session.userId = user._id;
-        var tokenString = auth.issueToken(username, role);
-        user.authToken = tokenString;
-        user.save(function (err, user) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error saving user ' + err
-                });
-            }
-            if (!user) {
-                return res.status(404).json({
-                    message: 'Unable to find user. User id: ' + user._id
-                });
-            }
+    if (!users) {
+      return res.status(404).json({
+        message: "Unable to find user. User Id: " + id
+      });
+    }
+    users.name = req.body.name;
+    users.username = req.body.username;
+    users.email = req.body.email;
+    users.locale = req.body.locale;
+    users.save(function(err, users) {
+      if (err) {
+        return res.status(500).json({
+          message: "Error saving user " + err
         });
-        return res.status(200).json({
-            token: tokenString,
-            message: "Token successfully generated"
+      }
+      if (!users) {
+        return res.status(404).json({
+          message: "Unable to find user. User id: " + id
         });
       }
     });
+    return res.status(200).json(users);
+  });
+}
+function loginUser(req, res) {
+  var role = req.swagger.params.role.value;
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (role !== "user" && role !== "admin") {
+    return res.status(400).json({
+      message: "Error: Role must be either admin or user"
+    });
+  }
+  User.authenticate(username, password, function(error, user) {
+    if (error || !user) {
+      return res.status(401).json({
+        message: "Wrong email or password."
+      });
+    } else {
+      req.session.userId = user._id;
+      var tokenString = auth.issueToken(username, role);
+      user.authToken = tokenString;
+      user.save(function(err, user) {
+        if (err) {
+          return res.status(500).json({
+            message: "Error saving user " + err
+          });
+        }
+        if (!user) {
+          return res.status(404).json({
+            message: "Unable to find user. User id: " + user._id
+          });
+        }
+      });
+      return res.status(200).json({
+        token: tokenString,
+        message: "Token successfully generated"
+      });
+    }
+  });
 }
 function logoutUser(req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    User.authenticate(username, password, function (error, user) {
-        if (error || !user) {
-            return res.status(401).json({
-                message: "Wrong email or password."
-            });
+  var username = req.body.username;
+  var password = req.body.password;
+  User.authenticate(username, password, function(error, user) {
+    if (error || !user) {
+      return res.status(401).json({
+        message: "Wrong email or password."
+      });
+    }
+    if (req.session) {
+      // delete session object
+      req.session.destroy(function(err) {
+        if (err) {
+          return res.status(500).json({
+            message: "Error removing session " + err
+          });
         }
-        if (req.session) {
-            // delete session object
-            req.session.destroy(function (err) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Error removing session ' + err
-                    });
-                }
-            });
-        }
-        user.authToken = '';
-        user.save(function (err, user) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error saving user ' + err
-                });
-            }
-            if (!user) {
-                return res.status(404).json({
-                    message: 'Unable to find user. User id: ' + user._id
-                });
-            }
+      });
+    }
+    user.authToken = "";
+    user.save(function(err, user) {
+      if (err) {
+        return res.status(500).json({
+          message: "Error saving user " + err
         });
-        return res.status(200).json({
-            message: "User successfully logout"
+      }
+      if (!user) {
+        return res.status(404).json({
+          message: "Unable to find user. User id: " + user._id
         });
+      }
     });
+    return res.status(200).json({
+      message: "User successfully logout"
+    });
+  });
 }
--- api/models/User.js
+++ api/models/User.js
@@ -1,73 +1,67 @@
-'use strict';
+"use strict";
 
-var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
-var constants = require('../constants');
+var mongoose = require("mongoose");
+var bcrypt = require("bcryptjs");
+var constants = require("../constants");
 var Schema = mongoose.Schema;
 
-const oAuthTypes = [
-    'github',
-    'twitter',
-    'facebook',
-    'google',
-    'linkedin'
-];
+const oAuthTypes = ["github", "twitter", "facebook", "google", "linkedin"];
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        default: ''
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-    },
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        default: ''
-    },
-    birthdate: { 
-        type: Date,
-        default: ''
-    },
-    provider: {
-        type: String,
-        default: ''
-    },
-    locale: {
-        type: String,
-        default: constants.DEFAULT_LANG
-    },
-    password: {
-        type: String,
-        required: true,
-        default: ''
-    },
-    authToken: {
-        type: String,
-        default: ''
-    },
-    lastlogin: {
-        type: Date,
-        default: Date.now
-    },
-    facebook: {
-        id: String,
-        token: String,
-        email: String,
-        name: String
-    },
-    google: {
-        id: String,
-        token: String,
-        email: String,
-        name: String
-    }
+  name: {
+    type: String,
+    default: ""
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    default: ""
+  },
+  birthdate: {
+    type: Date,
+    default: ""
+  },
+  provider: {
+    type: String,
+    default: ""
+  },
+  locale: {
+    type: String,
+    default: constants.DEFAULT_LANG
+  },
+  password: {
+    type: String,
+    required: true,
+    default: ""
+  },
+  authToken: {
+    type: String,
+    default: ""
+  },
+  lastlogin: {
+    type: Date,
+    default: Date.now
+  },
+  facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
+  },
+  google: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
+  }
 });
 
 const validatePresenceOf = value => value && value.length;
@@ -78,58 +72,50 @@ const validatePresenceOf = value => value && value.length;
 
 // the below 5 validations only apply if you are signing up traditionally
 
-userSchema.path('name').validate(function (name) {
-    if (this.skipValidation())
-        return true;
-    return name.length;
-}, 'Name cannot be blank');
+userSchema.path("name").validate(function(name) {
+  if (this.skipValidation()) return true;
+  return name.length;
+}, "Name cannot be blank");
 
-userSchema.path('email').validate(function (email) {
-    if (this.skipValidation())
-        return true;
-    return email.length;
-}, 'Email cannot be blank');
+userSchema.path("email").validate(function(email) {
+  if (this.skipValidation()) return true;
+  return email.length;
+}, "Email cannot be blank");
 
-userSchema.path('email').validate(function (email, fn) {
-    const User = mongoose.model('User');
-    if (this.skipValidation())
-        fn(true);
+userSchema.path("email").validate(function(email, fn) {
+  const User = mongoose.model("User");
+  if (this.skipValidation()) fn(true);
 
-    // Check only when it is a new user or when email field is modified
-    if (this.isNew || this.isModified('email')) {
-        User.find({email: email}).exec(function (err, users) {
-            fn(!err && users.length === 0);
-        });
-    } else
-        fn(true);
-}, 'Email already exists');
+  // Check only when it is a new user or when email field is modified
+  if (this.isNew || this.isModified("email")) {
+    User.find({ email: email }).exec(function(err, users) {
+      fn(!err && users.length === 0);
+    });
+  } else fn(true);
+}, "Email already exists");
 
-userSchema.path('username').validate(function (username) {
-    if (this.skipValidation())
-        return true;
-    return username.length;
-}, 'Username cannot be blank');
+userSchema.path("username").validate(function(username) {
+  if (this.skipValidation()) return true;
+  return username.length;
+}, "Username cannot be blank");
 
-userSchema.path('password').validate(function (password) {
-    if (this.skipValidation())
-        return true;
-    return password.length;
-}, 'Password cannot be blank');
-
+userSchema.path("password").validate(function(password) {
+  if (this.skipValidation()) return true;
+  return password.length;
+}, "Password cannot be blank");
 
 /**
  * Pre-save hook
  */
 
-userSchema.pre('save', function (next) {
-    if (!this.isNew)
-        return next();
+userSchema.pre("save", function(next) {
+  if (!this.isNew) return next();
 
-    if (!validatePresenceOf(this.password) && !this.skipValidation()) {
-        next(new Error('Invalid password'));
-    } else {
-        next();
-    }
+  if (!validatePresenceOf(this.password) && !this.skipValidation()) {
+    next(new Error("Invalid password"));
+  } else {
+    next();
+  }
 });
 
 /**
@@ -137,14 +123,13 @@ userSchema.pre('save', function (next) {
  */
 
 userSchema.methods = {
+  /**
+   * Validation is not required if using OAuth
+   */
 
-    /**
-     * Validation is not required if using OAuth
-     */
-
-    skipValidation: function () {
-        return ~oAuthTypes.indexOf(this.provider);
-    }
+  skipValidation: function() {
+    return ~oAuthTypes.indexOf(this.provider);
+  }
 };
 
 /**
@@ -152,52 +137,50 @@ userSchema.methods = {
  */
 
 userSchema.statics = {
+  /**
+   * Load
+   *
+   * @param {Object} options
+   * @param {Function} cb
+   * @api private
+   */
 
-    /**
-     * Load
-     *
-     * @param {Object} options
-     * @param {Function} cb
-     * @api private
-     */
+  load: function(options, cb) {
+    options.select = options.select || "name username";
+    return this.findOne(options.criteria)
+      .select(options.select)
+      .exec(cb);
+  },
 
-    load: function (options, cb) {
-        options.select = options.select || 'name username';
-        return this.findOne(options.criteria)
-                .select(options.select)
-                .exec(cb);
-    },
+  /**
+   * Authenticate input against database
+   *
+   * @param {String} username
+   * @param {String} password
+   * @param {Function} callback
+   * @api private
+   */
 
-    /**
-     * Authenticate input against database
-     *
-     * @param {String} username
-     * @param {String} password
-     * @param {Function} callback 
-     * @api private
-     */
-
-    authenticate: function (username, password, callback) {
-        this.findOne({username: username})
-                .exec(function (err, user) {
-                    if (err) {
-                        return callback(err);
-                    } else if (!user) {
-                        var err = new Error('User not found.');
-                        err.status = 401;
-                        return callback(err);
-                    }
-                    bcrypt.compare(password, user.password, function (err, result) {
-                        if (result === true) {
-                            return callback(null, user);
-                        } else {
-                            return callback();
-                        }
-                    });
-                });
-    }
+  authenticate: function(username, password, callback) {
+    this.findOne({ username: username }).exec(function(err, user) {
+      if (err) {
+        return callback(err);
+      } else if (!user) {
+        var err = new Error("User not found.");
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function(err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      });
+    });
+  }
 };
 
-var User = mongoose.model('User', userSchema);
+var User = mongoose.model("User", userSchema);
 
 module.exports = User;
