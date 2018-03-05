f4c3aebea8c268eb3b621ec6776b0a86f428e0f0
Amberley Romo <amberley@stream.me>
2018-03-04 19:13:18 -0600

    readme edits

--- app.js
+++ app.js
@@ -1,49 +1,55 @@
-'use strict';
+"use strict";
 
-var app = require('express')();
-var swaggerTools = require('swagger-tools');
-var YAML = require('yamljs');
+var app = require("express")();
+var swaggerTools = require("swagger-tools");
+var YAML = require("yamljs");
 var auth = require("./api/helpers/auth");
 var swaggerConfig = YAML.load("./api/swagger/swagger.yaml");
-var db = require('./db');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
+var db = require("./db");
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
 
-const config = require('./config');
+const config = require("./config");
 
 module.exports = app; // for testing
 
-swaggerTools.initializeMiddleware(swaggerConfig, function (middleware) {
-    //Serves the Swagger UI on /docs
-    app.use(middleware.swaggerMetadata()); // needs to go BEFORE swaggerSecurity
-    app.use(middleware.swaggerSecurity({
-        //manage token function in the 'auth' module
-        Bearer: auth.verifyToken
-        })
-    );
-    //use sessions for tracking logins
-    app.use(session({
-        secret: 'work hard',
-        resave: true,
-        saveUninitialized: false,
-        store: new MongoStore({
+swaggerTools.initializeMiddleware(swaggerConfig, function(middleware) {
+  // Base middleware that will analyze a request route,
+  // match it to an API in your Swagger document(s) and
+  // then annotate the request
+  app.use(middleware.swaggerMetadata()); // needs to go BEFORE swaggerSecurity
+
+  // This middleware allows you to wire up authentication/auth
+  // handlers based on the definitions in your Swagger document(s).
+  app.use(
+    middleware.swaggerSecurity({
+      //manage token function in the 'auth' module
+      Bearer: auth.verifyToken
+    })
+  );
+  //use sessions for tracking logins
+  app.use(
+    session({
+      secret: "work hard",
+      resave: true,
+      saveUninitialized: false,
+      store: new MongoStore({
         mongooseConnection: db
-        })
-    }));
-    
+      })
+    })
+  );
 
-    var routerConfig = {
-        controllers: "./api/controllers",
-        useStubs: false
-    };
+  var routerConfig = {
+    controllers: "./api/controllers",
+    useStubs: false
+  };
 
-    app.use(middleware.swaggerRouter(routerConfig));
+  app.use(middleware.swaggerRouter(routerConfig));
 
-    app.use(middleware.swaggerUi());
+  app.use(middleware.swaggerUi());
 
-    var port = process.env.PORT || 10010;
-    app.listen(port, function () {
-        console.log("Started server on port " + port);
-    });
+  var port = process.env.PORT || 10010;
+  app.listen(port, function() {
+    console.log("Started server on port " + port);
+  });
 });
-
