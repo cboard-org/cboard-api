7b022f59a79e884d21338260d83d9c822048212c
martin bedouret <martinbedouret@gmail.com>
2018-04-26 10:23:50 -0300

    initial draft for boards path

--- api/controllers/board.js
+++ api/controllers/board.js
@@ -48,5 +48,53 @@ function removeBoard(req, res) {
     return res.status(200).json(boards);
   });
 }
-function getBoard(req, res) {}
-function updateBoard(req, res) {}
+function getBoard(req, res) {
+  var id = req.swagger.params.id.value;
+  Board.findOne({ _id: id }, function(err, boards) {
+    if (err) {
+      return res.status(500).json({
+        message: 'Error getting board. ',
+        error: err
+      });
+    }
+    if (!boards) {
+      return res.status(404).json({
+        message: 'Board does not exist. Board Id: ' + id
+      });
+    }
+    return res.status(200).json(boards);
+  });
+}
+function updateBoard(req, res) {
+  var id = req.swagger.params.id.value;
+  Board.findOne({ _id: id }, function(err, board) {
+    if (err) {
+      return res.status(500).json({
+        message: 'Error updating board. ',
+        error: err
+      });
+    }
+    if (!board) {
+      return res.status(404).json({
+        message: 'Unable to find board. board Id: ' + id
+      });
+    }
+    for (var key in req.body) {
+      board[key] = req.body[key];
+    }
+    board.save(function(err, board) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error saving board. ',
+          error: err
+        });
+      }
+      if (!board) {
+        return res.status(404).json({
+          message: 'Unable to find board. board id: ' + id
+        });
+      }
+    });
+    return res.status(200).json(board);
+  });
+}
