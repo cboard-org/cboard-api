5c3fa227025a5f00a775f693f6dcff99d51f2ca8
martin bedouret <martinbedouret@gmail.com>
2018-05-18 12:41:45 -0300

    update smtp port

--- config/env/production.js
+++ config/env/production.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
 module.exports = {
-  env: "production",
-  databaseUrl: process.env.MONGO_URL || "mongodb://localhost/cboard-api",
+  env: 'production',
+  databaseUrl: process.env.MONGO_URL || 'mongodb://localhost/cboard-api',
   jwt: {
     secret: process.env.JWT_SECRET
   },
   emailTransport: {
-    from: "cboard@cboard.io",
-    host: "smtp.sendgrid.net",
-    port: 465,
+    from: 'cboard@cboard.io',
+    host: 'smtp.sendgrid.net',
+    port: 2325,
     secure: true,
-    service: "Sendgrid",
+    service: 'Sendgrid',
     auth: {
-      user: "apikey",
+      user: 'apikey',
       pass: process.env.SENDGRID_API_KEY
     }
   }
