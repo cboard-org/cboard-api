411e61a29257b2c61daf86a5bd00f156dfffa25b
Manuel Villafañe <manuel.villafane@engiefactory.com>
2019-03-19 18:40:54 -0300

    Add laguages model

---
+++ api/models/Laguages.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const constants = require('../constants');
+const Schema = mongoose.Schema;
+
+const LAGUAGES_SCHEMA_DEFINITION = {
+  locale: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  displayName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  skin: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  hair: {
+    type: String,
+    required: true,
+    trim: true
+  }
+};
+
+const laguagesSchema = new Schema(LAGUAGES_SCHEMA_DEFINITION);
+
+const Laguages = mongoose.model('Laguages', laguagesSchema);
+
+module.exports = Laguages;
