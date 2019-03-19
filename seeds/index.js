34203c9877231e27c180a92df26b984e51f104fd
Manuel Villafañe <manuel.villafane@engiefactory.com>
2019-03-19 18:44:15 -0300

    Add validate default data

--- seeds/index.js
+++ seeds/index.js
@@ -2,12 +2,15 @@ const LaguagesModel = require('../api/models/Laguages');
 const laguages = require('./laguages.json');
 
 module.exports = async () => {
-  console.log(`>>> Creating (${laguages.length}) laguages seed <<<`);
-  await Promise.all(
-    laguages.map(laguage => {
-      const newLaguage = new LaguagesModel(laguage);
-      return newLaguage.save();
-    })
-  );
-  console.log(`>>> (${laguages.length}) laguages seed created <<<`);
+  const laguagesDefaultData = await LaguagesModel.find();
+  if (laguagesDefaultData.length === 0) {
+    console.log(`>>> Creating (${laguages.length}) laguages seed <<<`);
+    await Promise.all(
+      laguages.map(laguage => {
+        const newLaguage = new LaguagesModel(laguage);
+        return newLaguage.save();
+      })
+    );
+    console.log(`>>> (${laguages.length}) laguages seed created <<<`);
+  }
 };
