22b3cc67a3afd73bd6a70e10e3413158d23d50c6
Manuel Villafañe <manuel.villafane@engiefactory.com>
2019-03-26 19:14:05 -0300

    Refacrtor in languages list response

--- api/controllers/languages.js
+++ api/controllers/languages.js
@@ -1,10 +1,19 @@
 const LanguagesModel = require('../models/Languages');
+const { paginatedResponse } = require('../helpers/response');
+const { getORQuery } = require('../helpers/query');
 
 const listLanguage = async (req, res) => {
   const { lang } = req.query;
-  const cond =
-    lang && lang.length ? { locale: { $regex: lang, $options: 'i' } } : {};
-  const laguages = await LanguagesModel.findOne(cond);
+  const searchFields = ['locale'];
+  const query = lang && lang.length ? getORQuery(searchFields, lang, true) : {};
+  const laguages = await paginatedResponse(
+    LanguagesModel,
+    {
+      query,
+      populate: ['communicators', 'boards']
+    },
+    req.query
+  );
   return res.status(200).json(laguages);
 };
 module.exports = {
