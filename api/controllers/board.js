d9122a0d09be2182ee9e4d27ddfed290980e3a33
Manuel Villafañe <manuel.villafane@engiefactory.com>
2019-04-08 14:51:14 -0400

    Add save board audios in azure

--- api/controllers/board.js
+++ api/controllers/board.js
@@ -1,6 +1,10 @@
 const { paginatedResponse } = require('../helpers/response');
 const { getORQuery } = require('../helpers/query');
 const Board = require('../models/Board');
+const azure = require('azure-storage');
+const blobService = azure.createBlobService();
+
+const BLOB_CONTAINER_NAME = process.env.BLOB_CONTAINER_NAME || 'cblob';
 
 module.exports = {
   createBoard: createBoard,
@@ -11,17 +15,70 @@ module.exports = {
   getBoardsEmail: getBoardsEmail
 };
 
-function createBoard(req, res) {
-  const board = new Board(req.body);
-  board.save(function(err, board) {
-    if (err) {
-      return res.status(409).json({
-        message: 'Error saving board',
-        error: err
-      });
-    }
-    return res.status(200).json(board.toJSON());
+async function createContainerIfNotExists(shareName) {
+  const result = await new Promise((resolve, reject) => {
+    blobService.createContainerIfNotExists(shareName, function(error, result) {
+      if (!error) {
+        resolve(result);
+      } else {
+        reject(error);
+      }
+    });
   });
+  return result;
+}
+
+async function createBlockBlobFromText({ fileName, buffer }) {
+  const result = await new Promise((resolve, reject) => {
+    blobService.createBlockBlobFromText(
+      BLOB_CONTAINER_NAME,
+      fileName,
+      buffer,
+      { contentSettings: { contentType: 'audio/mp4' } },
+      (error, result) => {
+        if (!error) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+  });
+
+  return result;
+}
+
+async function createBoard(req, res) {
+  const basePath = `${__dirname}/audio`;
+  const boardData = req.body;
+  try {
+    await createContainerIfNotExists(BLOB_CONTAINER_NAME);
+    const tiles = [];
+    for (const tile of boardData.tiles) {
+      if (tile.sound) {
+        let date = new Date();
+        date = Math.floor(date.getTime() / 1000);
+        const fileName = `${date}.mp4`;
+        const fileUploade = await createBlockBlobFromText({
+          fileName,
+          buffer: Buffer.from(tile.sound.split(';base64,').pop(), 'base64')
+        });
+        const audioPath = blobService.getUrl(
+          fileUploade.container,
+          fileUploade.name
+        );
+        tiles.push({ ...tile, sound: audioPath });
+      } else {
+        tiles.push(tile);
+      }
+    }
+    boardData.tiles = tiles;
+    const board = new Board(boardData);
+    await board.save();
+    return res.status(200).json(board.toJSON());
+  } catch (err) {
+    return res.status(500).json(err.message);
+  }
 }
 
 async function listBoard(req, res) {
