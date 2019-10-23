4240eedbe1f72efa9e2884ee1718df7b2140e043
Andrew Gilgallon <andrew@variancestudios.com>
2019-10-23 18:04:35 +0300

    Offline analytics upload endpoint

--- api/controllers/media.js
+++ api/controllers/media.js
@@ -1,11 +1,14 @@
 const uuidv1 = require('uuid/v1');
 const azure = require('azure-storage');
-const blobService = azure.createBlobService(process.env.AZURE_STORAGE_CONNECTION_STRING);
+const blobService = azure.createBlobService(
+  process.env.AZURE_STORAGE_CONNECTION_STRING
+);
 
 const BLOB_CONTAINER_NAME = process.env.BLOB_CONTAINER_NAME || 'cblob';
 
 module.exports = {
-  uploadMedia: uploadMedia
+  uploadMedia,
+  uploadAnalytics
 };
 
 async function uploadMedia(req, res) {
@@ -17,6 +20,21 @@ async function uploadMedia(req, res) {
       BLOB_CONTAINER_NAME,
       req.files.file[0]
     );
+    url = blobService.getUrl(file.container, file.name);
+  } catch (e) {}
+
+  res.status(url ? 200 : 500).json({ url });
+}
+
+async function uploadAnalytics(req, res) {
+  let url = null;
+
+  try {
+    await createContainerIfNotExists(BLOB_CONTAINER_NAME);
+    const file = await createBlockBlobFromText(BLOB_CONTAINER_NAME, {
+      ...req.files.file[0],
+      originalname: user.email
+    });
     url = blobService.getUrl(file.container, file.name);
   } catch (e) {}
 
