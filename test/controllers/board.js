1f4ed74d0892eb27b17ed8f92a6bd349e9eb9801
martin bedouret <martinbedouret@gmail.com>
2021-01-22 10:15:49 -0300

    wip

--- test/controllers/board.js
+++ test/controllers/board.js
@@ -17,6 +17,7 @@ const { token } = require('morgan');
 describe('Board API calls', function () {
 
   var authToken;
+  var boardId;
 
   before((done) => { //Before all we empty the database
     helper.prepareUser(server)
@@ -38,6 +39,54 @@ describe('Board API calls', function () {
         if (err) return done(err);
         // Verify response
         helper.verifyBoardProperties(res.body);
+        boardId = res.body.id;
+        done();
+      });
+  });
+
+  it('it should POST an empty board', function (done) {
+    const emptyBoard = { ...helper.boardData };
+    delete emptyBoard.tiles;
+    request(server)
+      .post('/board')
+      .send(emptyBoard)
+      .set('Authorization', 'Bearer ' + authToken)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        // Verify response
+        helper.verifyBoardProperties(res.body);
+        done();
+      });
+  });
+
+  it('it should NOT POST a board without name', function (done) {
+    const boardData = { ...helper.boardData };
+    delete boardData.name;
+    request(server)
+      .post('/board')
+      .send(boardData)
+      .set('Authorization', 'Bearer ' + authToken)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(409)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it('it should NOT POST a board without authorization', function (done) {
+    request(server)
+      .post('/board')
+      .send(helper.boardData)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(403)
+      .end(function (err, res) {
+        if (err) return done(err);
         done();
       });
   });
@@ -52,6 +101,25 @@ describe('Board API calls', function () {
         if (err) return done(err);
         // Verify response
         helper.verifyListProperties(res.body);
+        done();
+      });
+  });
+
+  it('it should PUT a board', function (done) {
+    const boardData = { ...helper.boardData };
+    boardData.name = 'edited name';
+    request(server)
+      .put('/board/' + boardId)
+      .send(boardData)
+      .set('Authorization', 'Bearer ' + authToken)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        // Verify response
+        helper.verifyBoardProperties(res.body);
+        res.body.name.should.to.equal(boardData.name);
         done();
       });
   });
