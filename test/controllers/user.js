aaaf21eeae2f6c317bbfd9b18772312039183054
Tomas Van Morlegan <tomivm98@gmail.com>
2021-03-21 22:31:42 -0300

    feedback results w/prettier

--- test/controllers/user.js
+++ test/controllers/user.js
@@ -1,12 +1,12 @@
 process.env.NODE_ENV = 'test';
 
-const request = require("supertest");
-const chai = require ('chai');
+const request = require('supertest');
+const chai = require('chai');
 var assert = chai.assert;
 
 const server = require('../../app');
 const helper = require('../helper');
-const { copy } = require("../../app");
+const { copy } = require('../../app');
 
 const nev = require('../../api/mail/index');
 
@@ -17,44 +17,43 @@ describe('User API calls', function () {
   let userid;
 
   before(async function (done) {
-    helper.deleteMochaUser(server)
-    .then(token => {
+    helper.deleteMochaUser(server).then((token) => {
       authToken = token;
       done();
     });
   });
-    
-  it("it should to create a new temporary user",function(done) {
+
+  it('it should to create a new temporary user', function (done) {
     this.timeout(5000); //to set timeout from 2s to 5s because the email server process take time.
     request(server)
       .post('/user')
       .send(helper.userData)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         const URLLenght = 16;
         if (err) done(err);
         url = res.body.url;
         url.should.be.a('string').with.lengthOf(URLLenght); //nev.options.URLLenght
         done();
-      }); 
-  })
+      });
+  });
 
-  it("it should to activate user",function(done) {
+  it('it should to activate user', function (done) {
     this.timeout(5000);
     request(server)
-    .post('/user/activate/' + url)
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .end(function(err, res){
-      if (err) done(err);
-      userid = res.body.userid;
-      userid.should.be.a('string');
-      userid.should.not.have.string(' '); //is not recomended negate assertions
-      done();
-    });
-  })
+      .post('/user/activate/' + url)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        if (err) done(err);
+        userid = res.body.userid;
+        userid.should.be.a('string');
+        userid.should.not.have.string(' '); //is not recomended negate assertions
+        done();
+      });
+  });
 
-  it("it should NOT Returns a valid token for a wrong email or password", function(done) {
+  it('it should NOT Returns a valid token for a wrong email or password', function (done) {
     const wrongUserData = { ...helper.userData };
     wrongUserData.password = 'wrongPassword';
     request(server)
@@ -62,22 +61,22 @@ describe('User API calls', function () {
       .send(wrongUserData)
       .expect('Content-Type', /json/)
       .expect(401)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
         authToken = res.body.authToken;
         (authToken === undefined).should.be.true;
-        (res.body.message).should.be.string;
+        res.body.message.should.be.string;
         done();
       });
   });
 
-  it("it should Returns a valid token for a user", function(done) {
+  it('it should Returns a valid token for a user', function (done) {
     request(server)
       .post('/user/login')
       .send(helper.userData)
       .expect('Content-Type', /json/)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
         authToken = res.body.authToken;
         authToken.should.be.a('string');
@@ -86,40 +85,40 @@ describe('User API calls', function () {
       });
   });
 
-  it("it should NOT Get the full users list without Bearer Token", function(done) {
+  it('it should NOT Get the full users list without Bearer Token', function (done) {
     request(server)
       .get('/user')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(403)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
         done();
       });
   });
 
-  it("it should Get the full users list", function(done) {
+  it('it should Get the full users list', function (done) {
     request(server)
       .get('/user')
       .set('Authorization', 'Bearer ' + authToken)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
         helper.verifyListProperties(res.body);
         done();
       });
   });
 
-  it("it should Get a specific user", function(done) {
+  it('it should Get a specific user', function (done) {
     request(server)
       .get('/user/' + userid)
       .set('Authorization', 'Bearer ' + authToken)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
         const getUser = res.body;
         getUser.should.to.have.any.keys('name', 'role', 'provider', 'email');
@@ -127,15 +126,15 @@ describe('User API calls', function () {
       });
   });
 
-  it("it should update a specific user", function(done) {
+  it('it should update a specific user', function (done) {
     request(server)
       .put('/user/' + userid)
-      .send({role: "admin"})
+      .send({ role: 'admin' })
       .set('Authorization', 'Bearer ' + authToken)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
         const updateUser = res.body;
         updateUser.should.to.have.property('role').to.equal('admin');
@@ -143,22 +142,22 @@ describe('User API calls', function () {
       });
   });
 
-  it("it should Destroys user session and authentication token", function(done) {
+  it('it should Destroys user session and authentication token', function (done) {
     request(server)
       .post('/user/logout')
-      .send(helper.userData) 
+      .send(helper.userData)
       .set('Authorization', 'Bearer ' + authToken)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
-        (res.body.message).should.be.string;
+        res.body.message.should.be.string;
         done();
       });
   });
 
-  it("it should NOT create a Clear token to auth for a wrong email", function(done) {
+  it('it should NOT create a Clear token to auth for a wrong email', function (done) {
     let wrongUserEmail = { ...helper.userData };
     wrongUserEmail.email = 'wrong_email@wrong.com';
     request(server)
@@ -166,22 +165,22 @@ describe('User API calls', function () {
       .send(wrongUserEmail)
       .expect('Content-Type', /json/)
       .expect(404)
-      .end(function(err, res){ 
+      .end(function (err, res) {
         if (err) done(err);
-        (res.body.message).should.be.a('string');
+        res.body.message.should.be.a('string');
         done();
       });
   });
 
-  it("it should create a Clear token to restore password", function(done) {
+  it('it should create a Clear token to restore password', function (done) {
     this.timeout(5000); //to await the email server process
     request(server)
       .post('/user/forgot')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .send(helper.userData) 
+      .send(helper.userData)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
         const userAndToken = res.body;
         userAndToken.should.be.a('object').with.any.keys('userid', 'token');
@@ -191,48 +190,48 @@ describe('User API calls', function () {
       });
   });
 
-  it("it should NOT allows to store a new password without a verification url.", function(done) {
+  it('it should NOT allows to store a new password without a verification url.', function (done) {
     let wrongVerificationUrl = { ...helper.userForgotPassword };
-    wrongVerificationUrl.token = "";
+    wrongVerificationUrl.token = '';
     request(server)
       .post('/user/store-password')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .send(wrongVerificationUrl) 
+      .send(wrongVerificationUrl)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
-        (res.body.message).should.be.a('string');
+        res.body.message.should.be.a('string');
         done();
       });
   });
 
-  it("it should allows to store a new password using a verification url.", function(done) {
+  it('it should allows to store a new password using a verification url.', function (done) {
     request(server)
       .post('/user/store-password')
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .send(helper.userForgotPassword) 
+      .send(helper.userForgotPassword)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
-        const storePasswordRes = (res.body);
+        const storePasswordRes = res.body;
         storePasswordRes.should.to.have.all.keys('success', 'message');
         done();
       });
   });
 
-  it("it should delete a user", function(done) {
+  it('it should delete a user', function (done) {
     request(server)
-      .del('/user/'+ helper.userForgotPassword.userid)
+      .del('/user/' + helper.userForgotPassword.userid)
       .set('Authorization', 'Bearer ' + authToken)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect(200)
-      .end(function(err, res){
+      .end(function (err, res) {
         if (err) done(err);
-        const userDeleted = (res.body);
-        userDeleted.should.to.have.any.keys('name', 'email' ,'locale');
+        const userDeleted = res.body;
+        userDeleted.should.to.have.any.keys('name', 'email', 'locale');
         done();
       });
   });
--- test/helper.js
+++ test/helper.js
@@ -8,159 +8,166 @@ const should = chai.should();
 
 const User = require('../api/models/User');
 
-const verifyListProperties = body => {
-    body.should.be.a('object');
-    body.should.to.have.all.keys('total','page', 'limit', 'offset', 'sort', 'search', 'data');
+const verifyListProperties = (body) => {
+  body.should.be.a('object');
+  body.should.to.have.all.keys(
+    'total',
+    'page',
+    'limit',
+    'offset',
+    'sort',
+    'search',
+    'data'
+  );
 };
 
-const verifyBoardProperties = body => {
-    body.should.be.a('object');
-    body.should.have.property('id');
-    body.should.have.property('name');
-    body.should.have.property('author');
-    body.should.have.property('email');
-    body.should.have.property('isPublic');
-    body.should.have.property('tiles');
+const verifyBoardProperties = (body) => {
+  body.should.be.a('object');
+  body.should.have.property('id');
+  body.should.have.property('name');
+  body.should.have.property('author');
+  body.should.have.property('email');
+  body.should.have.property('isPublic');
+  body.should.have.property('tiles');
 };
 
-const verifyUserProperties = user => {
-    user.should.be.a('object');
-    user.should.have.property('id');
-    user.should.have.property('name');
-    user.should.have.property('email');
-    user.should.have.property('password');
-}
+const verifyUserProperties = (user) => {
+  user.should.be.a('object');
+  user.should.have.property('id');
+  user.should.have.property('name');
+  user.should.have.property('email');
+  user.should.have.property('password');
+};
 
 const userData = {
-    name: "cboard mocha test",
-    email: "anything@cboard.io",
-    password: "123456"
+  name: 'cboard mocha test',
+  email: 'anything@cboard.io',
+  password: '123456',
 };
 
-let userid = "";
+let userid = '';
 
 let userForgotPassword = {
-    Userid: "",
-    token: "",
-    password: "newpassword"
+  Userid: '',
+  token: '',
+  password: 'newpassword',
 };
 
 const boardData = {
-    id: 'root',
-    name: 'home',
-    author: 'cboard mocha test',
-    email: 'anything@cboard.io',
-    isPublic: true,
-    hidden: false,
-    tiles: [
-        {
-            labelKey: 'cboard.symbol.yes',
-            image: '/symbols/mulberry/correct.svg',
-            id: 'HJVQMR9pX5F-',
-            backgroundColor: 'rgb(255, 241, 118)',
-            label: 'yes'
-        },
-        {
-            labelKey: 'symbol.descriptiveState.no',
-            image: '/symbols/mulberry/no.svg',
-            id: 'SkBQMRqpX5t-',
-            backgroundColor: 'rgb(255, 241, 118)',
-            label: 'no'
-        }
-    ]
+  id: 'root',
+  name: 'home',
+  author: 'cboard mocha test',
+  email: 'anything@cboard.io',
+  isPublic: true,
+  hidden: false,
+  tiles: [
+    {
+      labelKey: 'cboard.symbol.yes',
+      image: '/symbols/mulberry/correct.svg',
+      id: 'HJVQMR9pX5F-',
+      backgroundColor: 'rgb(255, 241, 118)',
+      label: 'yes',
+    },
+    {
+      labelKey: 'symbol.descriptiveState.no',
+      image: '/symbols/mulberry/no.svg',
+      id: 'SkBQMRqpX5t-',
+      backgroundColor: 'rgb(255, 241, 118)',
+      label: 'no',
+    },
+  ],
 };
 
 function prepareDb() {
-    mongoose.connect("mongodb://127.0.0.1:27017/cboard-api", {
-        useNewUrlParser: true
-    });
-    const connection = mongoose.connection;
+  mongoose.connect('mongodb://127.0.0.1:27017/cboard-api', {
+    useNewUrlParser: true,
+  });
+  const connection = mongoose.connection;
 
-    return new Promise((resolve, reject) => {
-        connection.once("open", function () {
-            mongoose.connection.db.dropDatabase(
-                function (err, result) {
-                    console.log("Database droped");
-                    resolve(true);
-                });
-        });
+  return new Promise((resolve, reject) => {
+    connection.once('open', function () {
+      mongoose.connection.db.dropDatabase(function (err, result) {
+        console.log('Database droped');
+        resolve(true);
+      });
     });
+  });
 }
 
 function prepareUser(server) {
-    var token;
-    var url;
-    return new Promise((resolve, reject) => {
+  var token;
+  var url;
+  return new Promise((resolve, reject) => {
+    request(server)
+      .post('/user')
+      .send(userData)
+      .expect(200)
+      .expect(function (res) {
+        url = res.body.url;
+      })
+      .end(function () {
         request(server)
-            .post('/user')
-            .send(userData)
-            .expect(200)
-            .expect(function (res) {
-                url = res.body.url;
-            })
-            .end(function () {
-                request(server)
-                    .post('/user/activate/' + url)
-                    .send('')
-                    .expect(200)
-                    .end(function (err,res) {
-                        userid = res.body.userid;
-                        request(server)
-                            .post('/user/login')
-                            .send(userData)
-                            .expect(200)
-                            .expect(function (res) {
-                                token = res.body.authToken;
-                            })
-                            .end(function () {
-                                resolve(token, userid);
-                            });
-                    });
-            });
-    });
+          .post('/user/activate/' + url)
+          .send('')
+          .expect(200)
+          .end(function (err, res) {
+            userid = res.body.userid;
+            request(server)
+              .post('/user/login')
+              .send(userData)
+              .expect(200)
+              .expect(function (res) {
+                token = res.body.authToken;
+              })
+              .end(function () {
+                resolve(token, userid);
+              });
+          });
+      });
+  });
 }
 
 function deleteMochaUser(server) {
-    let authToken; 
-    return new Promise((resolve, reject) => {
-        User.findOne({ email: userData.email }, function (err, user) {
-            userid = user._id
-            request(server)
-                .post('/user/login')
-                .send(userData)
+  let authToken;
+  return new Promise((resolve, reject) => {
+    User.findOne({ email: userData.email }, function (err, user) {
+      userid = user._id;
+      request(server)
+        .post('/user/login')
+        .send(userData)
+        .expect(200)
+        .expect(function (res) {
+          authToken = res.body.authToken;
+        })
+        .end(function () {
+          request(server)
+            .put('/user/' + userid)
+            .set('Authorization', 'Bearer ' + authToken)
+            .send({ role: 'admin' })
+            .end(function () {
+              request(server)
+                .del('/user/' + userid)
+                .set('Authorization', 'Bearer ' + authToken)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
                 .expect(200)
-                .expect(function (res) {
-                    authToken = res.body.authToken;
-                })
-                .end(function(){
-                    request(server)
-                        .put('/user/' + userid)
-                        .set('Authorization', 'Bearer ' + authToken)
-                        .send({role: "admin"})
-                        .end(function(){
-                            request(server)
-                                .del('/user/'+ userid)
-                                .set('Authorization', 'Bearer ' + authToken)
-                                .set('Accept', 'application/json')
-                                .expect('Content-Type', /json/)
-                                .expect(200)
-                                .end(function () {
-                                    resolve();
-                                });
-                        })    
-                });  
-        })    
-    });      
+                .end(function () {
+                  resolve();
+                });
+            });
+        });
+    });
+  });
 }
 
 module.exports = {
-    verifyListProperties,
-    verifyBoardProperties,
-    verifyUserProperties,
-    prepareDb,
-    prepareUser,
-    deleteMochaUser,
-    boardData,
-    userData,
-    userForgotPassword
+  verifyListProperties,
+  verifyBoardProperties,
+  verifyUserProperties,
+  prepareDb,
+  prepareUser,
+  deleteMochaUser,
+  boardData,
+  userData,
+  userForgotPassword,
 };
