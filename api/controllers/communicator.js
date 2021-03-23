d2155ac8baa15f27ffc9921f16776101931c67db
Tomas Van Morlegan <tomivm98@gmail.com>
2021-03-23 16:25:09 -0300

    double delete bug fix

--- api/controllers/communicator.js
+++ api/controllers/communicator.js
@@ -145,7 +145,12 @@ function removeCommunicator(req, res) {
         error: err.message
       });
     }
-
+    if (!communicator) {
+      return res.status(404).json({
+        message: 'Communicator not found. Communicator Id: ' + id,
+        error: 'Communicator not found.'
+      });
+    }
     return res.status(200).json(communicator);
   });
 }
