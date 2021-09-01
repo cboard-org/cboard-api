e4d1ef4056aba60dec29b3d04fafcf0ebd91ce49
Tomas Van Morlegan <tomivm98@gmail.com>
2021-09-01 10:20:50 -0300

    urljoin in all CallbackURL properties and remove return in arrows fn

--- api/passport/facebook.js
+++ api/passport/facebook.js
@@ -14,22 +14,22 @@ const FBStrategy = {
 passport.use(new FacebookStrategy(FBStrategy, UserController.facebookLogin));
 
 const configureFacebookStrategy = app => {
-  app.get('/login/facebook', (req, res, next) => {
-    return passport.authenticate('facebook', {
+  app.get('/login/facebook', (req, res, next) => 
+    passport.authenticate('facebook', {
       session: false,
       scope: config.facebook.SCOPE,
       callbackURL: urljoin(req.domain, config.facebookCallbackPath)
-    })(req, res, next);
-  });
+    })(req, res, next)
+  );
 
   app.get(
     '/login/facebook/callback',
-    (req, res, next) => {
-      return passport.authenticate('facebook', {
+    (req, res, next) => 
+      passport.authenticate('facebook', {
         session: false,
-        callbackURL: req.domain + config.facebookCallbackPath
-      })(req, res, next);
-    },
+        callbackURL: urljoin(req.domain, config.facebookCallbackPath)
+      })(req, res, next)
+    ,
     (req, res) => {
       res.json(req.user);
     }
