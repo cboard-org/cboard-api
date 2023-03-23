56ac780b869d6cb0f20d403eeac98a2dea6cdb41
Martin Bedouret <martinbedouret@gmail.com>
2023-03-23 17:21:13 -0300

    Removed permission for syncronize

--- api/controllers/subscription.js
+++ api/controllers/subscription.js
@@ -206,7 +206,7 @@ async function syncSubscriptions(req, res) {
       }
     });
 
-    return res.status(200).json(localSubscrs);
+    return res.status(200).json();
   } catch (err) {
     console.log(err.message);
     return res.status(500).json({
