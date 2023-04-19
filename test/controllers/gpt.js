e82306bc87c1ae9c9e312ea09daa58d74bd12e9d
tomivm <tomivm98@gmail.com>
2023-04-19 13:04:42 -0300

    format GPT test

--- test/controllers/gpt.js
+++ test/controllers/gpt.js
@@ -5,46 +5,46 @@ const helper = require('../helper');
 const nock = require('nock');
 
 //Parent block
-describe('GPT API calls', function () {
+describe('GPT API calls', function() {
   let user;
   let server;
 
-  before(async function () {
+  before(async function() {
     helper.prepareNodemailerMock(); //enable mockery and replace nodemailer with nodemailerMock
     server = require('../../app'); //register mocks before require the original dependency
     helper.userData.email = helper.generateEmail();
     user = await helper.prepareUser(server, {
       role: 'user',
-      email: helper.userData.email,
+      email: helper.userData.email
     });
   });
 
-  after(async function () {
+  after(async function() {
     helper.prepareNodemailerMock(true); //disable mockery
     await helper.deleteMochaUsers();
   });
 
-  describe('GET /gpt/edit', function () {
+  describe('GET /gpt/edit', function() {
     const toEditData = helper.gpt.toEditData;
 
-    it('it should return error if user is not logged.', async function () {
-        const res = await request(server)
-          .get('/gpt/edit')
-          .send(toEditData)
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(403);
-      });
+    it('it should return error if user is not logged.', async function() {
+      const res = await request(server)
+        .get('/gpt/edit')
+        .send(toEditData)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(403);
+    });
 
-    it('it should return error if phrase is not completed.', async function () {
-        const res = await request(server)
-          .get('/gpt/edit')
-          .send({})
-          .set('Authorization', `Bearer ${user.token}`)
-          .set('Accept', 'application/json')
-          .expect('Content-Type', /json/)
-          .expect(400);
-      });
+    it('it should return error if phrase is not completed.', async function() {
+      const res = await request(server)
+        .get('/gpt/edit')
+        .send({})
+        .set('Authorization', `Bearer ${user.token}`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400);
+    });
 
     it('it should improve provided phrase and return it.', async function() {
       const mockedOpenAPIResponse = {
