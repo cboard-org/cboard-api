ade36323229580f95477b165d129c4b45376ff67
tomivm <tomivm98@gmail.com>
2023-05-04 14:56:25 -0300

    Prevent crash during create a transaction

--- api/models/Subscribers.js
+++ api/models/Subscribers.js
@@ -173,7 +173,7 @@ subscribersSchema.path('transaction').validate(async function (transaction) {
   } else if (transaction.platform === 'paypal') {
     await verifyPaypalPurchase(transaction.nativePurchase);
   }
-  this.markModified('transaction');
+  this?.markModified('transaction');
   return true;
 }, 'transaction puchase token error');
 
