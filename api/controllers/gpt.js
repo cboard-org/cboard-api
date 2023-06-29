5c2970f4504adc61532939ce160d9968a8e14659
tomivm <tomivm98@gmail.com>
2023-06-29 12:30:28 -0300

    Rename api key environment variable

--- api/controllers/gpt.js
+++ api/controllers/gpt.js
@@ -1,5 +1,5 @@
 const { Configuration, OpenAIApi } = require('openai');
-const apiKey = process.env.OPENAI_API_KEY;
+const apiKey = process.env.AZURE_OPENAI_API_KEY;
 
 const configuration = new Configuration({
   apiKey,
