0ce6a0c3b7af282cf59b3bbf2712bbb51922513e
tomivm <tomivm98@gmail.com>
2023-09-26 13:01:04 -0300

    Manage grace IOS grace period

--- api/helpers/appStore.js
+++ api/helpers/appStore.js
@@ -23,8 +23,24 @@ const verifyReceipt = async (encodedAppStoreReceipt, isSandbox = false) => {
     const res = await axios.post(verifyReceiptUrl, body, {
       headers
     });
-    if (res?.data?.latest_receipt_info && res?.data?.latest_receipt_info['0'])
-      return res.data?.latest_receipt_info['0'];
+    if (res?.data?.latest_receipt_info && res?.data?.latest_receipt_info['0']) {
+      const isInGracePeriod =
+        res?.data?.pending_renewal_info[0]?.is_in_billing_retry_period === '1';
+
+      if (isInGracePeriod) {
+        const pending_renewal_info = res.data.pending_renewal_info[0];
+        const grace_period_info = {
+          is_in_billing_retry_period: 1,
+          grace_period_expires_date_ms:
+            pending_renewal_info.grace_period_expires_date_ms
+        };
+        return { ...res.data.latest_receipt_info['0'], ...grace_period_info };
+      }
+      return {
+        ...res.data.latest_receipt_info['0'],
+        is_in_billing_retry_period: 0
+      };
+    }
     const SANDBOX_RESPONSE_STATUS = 21007;
     isSandboxReceipt = res?.data?.status === SANDBOX_RESPONSE_STATUS;
     if (isSandboxReceipt) {
@@ -46,10 +62,22 @@ const setSubscriptionState = transaction => {
 
   const nowMs = moment().valueOf();
   const expires_date_ms = parseInt(transaction.expires_date_ms);
-  if (transaction.cancellation_date_ms) {
+  const gracePeriodExpiresDateMs = parseInt(
+    transaction.grace_period_expires_date_ms
+  );
+  if (
+    transaction.is_in_billing_retry_period &&
+    nowMs <= gracePeriodExpiresDateMs
+  ) {
+    return IN_GRACE_PERIOD;
+  }
+  if (
+    transaction.cancellation_date_ms &&
+    transaction.is_in_billing_retry_period
+  ) {
     const cancellationDateMs = parseInt(transaction.cancellation_date_ms);
     if (nowMs >= cancellationDateMs) return EXPIRED;
-    setExpireDate(cancellationDateMs);
+    //setExpireDate(cancellationDateMs);
     return CANCELLED;
   }
   if (nowMs <= expires_date_ms) return ACTIVE;
@@ -75,7 +103,15 @@ const verifyAppStorePurchase = async ({ appStoreReceipt }) => {
   try {
     const decodedReceipt = await verifyReceipt(appStoreReceipt);
     transaction = { ...transaction, ...decodedReceipt };
+
     setExpireDate(parseInt(decodedReceipt.expires_date_ms));
+
+    if (decodedReceipt.is_in_billing_retry_period === 1) {
+      const gracePeriodExpiresDateMs = parseInt(
+        transaction.grace_period_expires_date_ms
+      );
+      setExpireDate(gracePeriodExpiresDateMs);
+    }
 
     transaction.subscriptionState = setSubscriptionState(transaction);
     return transaction;
