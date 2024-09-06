019ff3c4cebf9d52463b4afa8a4b36aafafe8fea
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2024-09-06 11:12:24 -0300

    Refactor getCbuilderBoardbyId function and add cbuilderToCboardTilesAdapter helper

--- api/helpers/cbuilder.js
+++ api/helpers/cbuilder.js
@@ -5,10 +5,18 @@ const { INTERNAL_API_KEY, CBUILDER_API_URL } = config;
 async function getCbuilderBoardbyId(id) {
   const res = await axios.get(CBUILDER_API_URL + '/api/board/' + id, {
     headers: {
-      'Authorization': `Bearer ${INTERNAL_API_KEY}`
+      Authorization: `Bearer ${INTERNAL_API_KEY}`
     }
   });
-  return res.data;
+  //filter out Cbuilder generated tiles
+  const tiles = cbuilderToCboardTilesAdapter(res.data?.tiles) || [];
+  const board = { ...res.data, tiles: tiles };
+
+  return board;
+}
+
+function cbuilderToCboardTilesAdapter(tiles) {
+  return tiles.map(({suggestedImages, generatedPicto, ...tile}) => tile);
 }
 
 module.exports = {
