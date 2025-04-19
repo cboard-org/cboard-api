4dcbfef1fbc39fcea8dcf0ff004a4032405010f2
martin bedouret <martinbedouret@gmail.com>
2025-04-19 20:12:44 -0300

    Update azure storage package to @azure/blob-storage package

--- api/helpers/blob.js
+++ api/helpers/blob.js
@@ -1,67 +1,43 @@
+const { BlobServiceClient } = require('@azure/storage-blob');
 const uuidv1 = require('uuid/v1');
-const azure = require('azure-storage');
-const blobService = azure.createBlobService(
+
+const blobServiceClient = BlobServiceClient.fromConnectionString(
   process.env.AZURE_STORAGE_CONNECTION_STRING
 );
 
 module.exports = {
-  createBlockBlobFromText
+  createBlockBlobFromText,
 };
 
-function createContainerIfNotExists(shareName) {
-  return new Promise((resolve, reject) => {
-    blobService.createContainerIfNotExists(shareName, function(error, result) {
-      if (!error) {
-        resolve(result);
-      } else {
-        reject(error);
-      }
-    });
-  });
+async function createContainerIfNotExists(containerName) {
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const createResponse = await containerClient.createIfNotExists();
+  return createResponse.succeeded;
 }
 
-// Returns [file:BlobResult, fileUrl:string]
-async function createBlockBlobFromText(
-  containerName,
-  fileName,
-  file,
-  prefix = ''
-) {
+// Returns [file:BlockBlobClient, fileUrl:string]
+async function createBlockBlobFromText(containerName, fileName, file, prefix = '') {
   await createContainerIfNotExists(containerName);
 
   const { buffer, mimetype } = file;
 
+  const ts = Math.round(new Date().getTime() / 1000);
+  const uuidSuffix = uuidv1().split('-').pop();
+  const finalName = `${prefix}_${ts}_${uuidSuffix}_${fileName.toLowerCase().trim()}`;
+
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const blockBlobClient = containerClient.getBlockBlobClient(finalName);
+
   const options = {};
   if (mimetype && mimetype.length) {
     const cacheMaxAgeInSeconds = 31536000;
-    options.contentSettings = {
-      contentType: mimetype,
-      cacheControl: `max-age=${cacheMaxAgeInSeconds}`
+    options.blobHTTPHeaders = {
+      blobContentType: mimetype,
+      blobCacheControl: `max-age=${cacheMaxAgeInSeconds}`,
     };
   }
 
-  const ts = Math.round(new Date().getTime() / 1000);
-  const uuidSuffix = uuidv1()
-    .split('-')
-    .pop();
-  const finalName = `${prefix}_${ts}_${uuidSuffix}_${fileName
-    .toLowerCase()
-    .trim()}`;
+  await blockBlobClient.upload(buffer, buffer.length, options);
 
-  return new Promise((resolve, reject) => {
-    blobService.createBlockBlobFromText(
-      containerName,
-      finalName,
-      buffer,
-      options,
-      function(error, file) {
-        if (error) {
-          reject(error);
-          return;
-        }
-
-        resolve([file, blobService.getUrl(file.container, file.name)]);
-      }
-    );
-  });
+  return [blockBlobClient, blockBlobClient.url];
 }
