98e5e16c7b9ca473e9f19b9cda28d672c26a0d2f
martin bedouret <martinbedouret@gmail.com>
2026-08-09 20:00:45 -0300

    Escape user input for regular expression queries
    Properly escape metacharacters in user-supplied search values to prevent regex injection and ReDoS vulnerabilities via crafted patterns.

--- api/helpers/query.js
+++ api/helpers/query.js
@@ -3,8 +3,16 @@ module.exports = {
   getORQuery: getORQuery
 };
 
+// Escape regex metacharacters so user-supplied search values are matched
+// literally. This prevents regex injection and ReDoS via crafted patterns.
+function escapeRegExp(value) {
+  return String(value).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
 function getQuery(property, value, regex = false) {
-  const queryValue = regex ? { $regex: new RegExp(value, 'ig') } : value;
+  const queryValue = regex
+    ? { $regex: new RegExp(escapeRegExp(value), 'ig') }
+    : value;
 
   return { [property]: queryValue };
 }
